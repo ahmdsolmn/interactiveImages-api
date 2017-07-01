6a7c80f1c43d20486f56d2f6f6e3ef9e2394e4b5
ahmdsolmn <ahmdsolmn@gmail.com>
2017-07-01 11:57:19 +0200

    handle images

---
+++ src/models/images.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// this should be retrieved from a database
+const IMAGES = {
+    'e1337h': 'http://www.timvandevall.com/wp-content/uploads/plant-cell-diagram.jpg',
+}
+
+module.exports = {
+    getImage: imageId => {
+        if(undefined === IMAGES[imageId]) {
+            return Promise.reject('image_not_found');
+        } else {
+            return Promise.resolve(IMAGES[imageId]);
+        }
+    }
+
+    // optional
+    // app.get('/images.set', (req, res) => {
+        
+    // });
+}

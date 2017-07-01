237d7d476cc808c608595a60d65f3a61813b293c
ahmdsolmn <ahmdsolmn@gmail.com>
2017-07-01 12:52:23 +0200

    bug fix

--- src/routes/taps.js
+++ src/routes/taps.js
@@ -4,17 +4,17 @@ let tapsModel = require('../models/taps');
 
 module.exports = app => {
     app.get('/taps.get', (req, res) => {
-        return tapsModel.getAllAndCurrent(req.query.image_id, req.query.image_id)
+        return tapsModel.getAllAndCurrent(req.query.image_id, req.query.user_id)
             .then(data => {
                 return res.send({
                     ok: true,
-                    data: TAPS[req.query.image_id],
+                    data: data,
                 });
             })
             .catch(err => {
                 return res.send({
                     ok: false,
-                    data: err,
+                    error: err,
                 });    
             });
     });
